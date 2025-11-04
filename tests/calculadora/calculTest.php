4ab2cd3060dd0071cafc5ea148cd30340da7f4c4
Fran_27 <27francis3@gmail.com>
2025-11-03 19:41:30 -0600

    Listo ya funciona, pero nose porque me marca error en la prueba de la resta

--- tests/calculadora/calculTest.php
+++ tests/calculadora/calculTest.php
@@ -1,12 +1,30 @@
 <?php
 
 require_once 'clase.php';
-$SUMA= new Calculadora();
+$Ope= new Calculadora();
 
+$a=5;
+$b=5;
 
+$SUMA_RESULTADO = $Ope->sumar($a,$b);
+$RESTA_RESULTADO = $Ope->restar($aa,$b);
+$MULTIPLICACION_RESULTADO = $Ope->multiplicar($a,$b);
+$DIVISION_RESULTADO = $Ope->dividir($a,$b);
 
-$SUMA_RESULTADO = $SUMA->sumar(1,1);
 
 test('SUMA', function () use ($SUMA_RESULTADO) {
-    expect($SUMA_RESULTADO)->toBe(2);
+    expect($SUMA_RESULTADO)->toBe(10);
 });
+
+test('RESTA', function () use ($RESTA_RESULTADO) {
+    expect($RESTA_RESULTADO)->toBe(0);
+});
+
+test('MULTIPLICACION', function () use ($MULTIPLICACION_RESULTADO) {
+    expect($MULTIPLICACION_RESULTADO)->toBe(25);
+});
+
+test('DIVISION', function () use ($DIVISION_RESULTADO) {
+    expect($DIVISION_RESULTADO)->toBe(1);
+});
+
